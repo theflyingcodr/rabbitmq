72f220955585f336bc234efbc831b007f5e2f0bd
Mark Smith <mark@azert.co.uk>
2018-05-24 22:52:05 +0100

    queue now building and binding to exchange with default configs setup

--- consumer.go
+++ consumer.go
@@ -2,12 +2,89 @@ package messaging
 
 import (
 	"context"
+	"github.com/pborman/uuid"
+	"fmt"
 )
 
 type ConsumerConfig struct{
 	Name string
-	Durable bool
-	Ttl int64
+	Durable *bool
+	AutoDelete *bool
+	NoWait *bool
+	Exclusive *bool
+	Ttl *uint
+	PrefetchCount *uint
+	PrefetchSize *uint
+	Args map[string]interface{}
+}
+
+// GetName returns the consumer name if set in config
+// otherwise it returns a random uuid
+func(c *ConsumerConfig) GetName() string{
+	if c.Name == ""{
+		return uuid.NewUUID().String()
+	}
+
+	return fmt.Sprintf("%s-%s",c.Name, uuid.NewUUID().String())
+}
+
+// GetDurable returns the type of durability
+// set in config, if nil then it returns a
+// default of true
+func(c *ConsumerConfig) GetDurable() bool{
+	if c.Durable == nil{
+		return true
+	}
+	return *c.Durable
+}
+
+// GetPrefetchCount returns the Qos value for number
+// of messages pulled from the queue at a time
+// default is 0 which will pull the default count for most libs
+func(c *ConsumerConfig) GetPrefetchCount() uint{
+	if c.PrefetchCount == nil{
+		return 0
+	}
+
+	return *c.PrefetchCount
+}
+
+func(c *ConsumerConfig) GetPrefetchSize() uint{
+	if c.PrefetchSize == nil{
+		return 0
+	}
+
+	return *c.PrefetchSize
+}
+
+// GetAutoDelete determines whether the queue is deleted
+// on server restart, default is false
+func(c *ConsumerConfig) GetAutoDelete() bool{
+	if c.AutoDelete == nil{
+		return false
+	}
+	return *c.AutoDelete
+}
+
+// GetNoWait When true, the queue will assume to be declared on the server.  A
+//channel exception will arrive if the conditions are met for existing queues
+//or attempting to modify an existing queue from a different connection.
+// default is false
+func(c *ConsumerConfig) GetNoWait() bool{
+	if c.NoWait == nil{
+		return false
+	}
+	return *c.NoWait
+}
+
+// GetExclusive queues are only accessible by the connection that declares them and
+//will be deleted when the connection closes.
+// default is false
+func(c *ConsumerConfig) GetExclusive() bool{
+	if c.Exclusive == nil{
+		return false
+	}
+	return *c.Exclusive
 }
 
 // Consumer is an interface which can be implemented
--- rabbitmq/host.go
+++ rabbitmq/host.go
@@ -3,8 +3,8 @@ package rabbitmq
 import (
 	"github.com/streadway/amqp"
 	"github.com/azert-software/amqp"
-	"fmt"
-	"github.com/teamwork/log"
+	log "github.com/Sirupsen/logrus"
+	"context"
 )
 
 
@@ -33,10 +33,6 @@ func (h *RabbitHost) Init(cfg *messaging.HostConfig) (err error){
 // AddBroker will register an exchange and n consumers
 // which will consume from that exchange
 func (h *RabbitHost) AddBroker(cfg *messaging.ExchangeConfig, consumers []messaging.Consumer) error {
-	n, err := cfg.GetName()
-	if err != nil{
-		return err
-	}
 	h.brokers = append(h.brokers, Broker{exchange:cfg, consumers:consumers})
 
 	return nil
@@ -47,11 +43,45 @@ func (h *RabbitHost) AddBroker(cfg *messaging.ExchangeConfig, consumers []messag
 func (h *RabbitHost) Run() (err error){
 	ch, err := h.connection.Channel()
 	if err != nil{
-		return err
+		return
 	}
 	for _, b := range h.brokers{
+		n, err := b.exchange.GetName()
+		if err != nil{
+			return
+		}
 		h.BuildExchange(ch, b)
 		for _, c := range b.consumers {
+			cfg, err := c.Init()
+			if err != nil{
+				return
+			}
+
+			for k, r := range c.Setup(context.Background()){
+				go func(){
+					// each consumer has its own channel & each queue has its own consumer
+					queueChannel, err := h.connection.Channel()
+					if err != nil{
+						return
+					}
+
+					if err = queueChannel.Qos(int(cfg.GetPrefetchCount()), int(cfg.GetPrefetchSize()), false); err != nil{
+						return
+					}
+					_, err = queueChannel.QueueDeclare(cfg.GetName(), cfg.GetDurable(), cfg.GetAutoDelete(), cfg.GetExclusive(), cfg.GetNoWait(), cfg.Args)
+					if err != nil{
+						return
+					}
+					for _, key := range r.Keys{
+						if err = queueChannel.QueueBind(k, key, n, cfg.GetNoWait(), cfg.Args ); err != nil{
+							return
+						}
+
+
+					}
+				}()
+
+			}
 
 		}
 	}
@@ -66,12 +96,12 @@ func (h *RabbitHost) BuildExchange(ch *amqp.Channel, b Broker) (err error){
 	if err != nil{
 		return err
 	}
+
 	log.Debugf("setting up %s exchange", n)
 	if err = ch.ExchangeDeclare(n, ex.GetType(), ex.GetDurable(), ex.GetAutoDelete(), ex.GetInternal(), false, ex.GetArgs()); err != nil{
-		log.Errorf(err, "error when setting up exhange %s",n)
+		log.Errorf("error when setting up exchange %s: %s",n, err.Error())
 		return
 	}
-
 	log.Debugf("%s exchange setup success", n)
 	return
 }
